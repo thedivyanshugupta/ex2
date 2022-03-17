59409f6c6637044a045994f8efff5b911326aaf2
project <divyanshu.gupta@roro.io>
2022-03-17 12:57:32 +0530

    Revert "update name.java"
    This reverts commit db9a7d1070a3984da11da39abd73d2895700fd03.

--- name.java
+++ name.java
@@ -2,7 +2,6 @@ public class name
 {
     public static void main(String[] args)
     {
-        System.out.println("name is ex2");
-        System.out.println("iiii")
+        System.out.println("ex2");
     }
 }
